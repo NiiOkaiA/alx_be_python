1f9dee5087e23195e6068a8a7c95c41d167c513b
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-07-10 22:28:51 +0000

    commit first finance calculator

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,8 @@
+monthly_income= int(input("enter your monthly income: "))
+total_expenses= int (input("enter total monthly expenses: "))
+monthly_savings = monthly_income - total_expenses
+
+projected_savings= monthly_savings * 12 + (monthly_savings*12*0.05)
+
+print("monthly savings", monthly_savings)
+print("projected annual savings",projected_savings)

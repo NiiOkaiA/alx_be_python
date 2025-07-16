b1d3021bf7dc5431f72de228dba2dafaaeab0c34
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-07-16 04:37:14 +0000

    first commit of pattern drawing

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,9 @@
+square=int(input("enter size of square pattern:"))
+number=square
+while square >0:
+    for a in range(number):
+        print("*",end="")
+    print("\n")
+    square=square-1
+    
+    

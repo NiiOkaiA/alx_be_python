8979887a372d816650e0ba42f6815819a4119698
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-07-17 05:58:24 +0000

    capitalized Choose in operation variable

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,7 +1,7 @@
 num1=int(input("Enter the first number:"))
 num2=int(input("Enter the second number:"))
 
-operation=input("choose the operation(+,-,*,/):")
+operation=input("Choose the operation(+,-,*,/):")
 
 if operation =="+":
     result= num1+num2
@@ -11,7 +11,7 @@ elif operation=="*":
     result=num1*num2
 elif operation=="/" :
     if num2==0:
-        print("no division by 0")
+        print("cannot divide by 0")
         exit()
     result=num1/num2
 else:

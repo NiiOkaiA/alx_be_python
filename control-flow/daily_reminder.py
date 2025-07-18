192535ec40be98fabbe7b15c346ded60387dc4de
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-07-18 06:58:25 +0000

    matched priority

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,8 +5,13 @@ Priority=input("Priority (high/medium/low):")
 Time_Bound=input("Is it time-bound?  (yes/no):")
 
 
-match time:
+match priority:
     case "yes":
         print(task, "is a",priority,"priority task that requires your attention today")
     case "no":
          print(task, "is a",priority,"priority task. Consider completing it when you have time")
+
+
+'''match priority:
+    case "high":
+          print(task, "is a",priority,"priority task that requires your attention today")'''

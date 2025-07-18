2879c2c44003ca4a9ca4bec9d38fbc17bd4b0ddc
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-07-18 07:31:10 +0000

    added cannot divide by o

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,9 @@
 num1=int(input("Enter the first number:"))
 num2=int(input("Enter the second number:"))
 
+if num2==0:
+        print("Cannot divide by 0")
+        exit()
 operation=input("Choose the operation (+, -, *, /):")
 
 
@@ -8,13 +11,13 @@ operation=input("Choose the operation (+, -, *, /):")
 match operation:
         case "+":
              result=num1+num2
-             print ("The result is",result)
+             print ( "The result is",result)
         case "-":
              result=num1-num2
-             print ("The result is",result)
+             print ( "The result is",result)
         case "*":
              result=num1*num2
-             print ("The result is",result)
+             print ( "The result is",result)
         case "/":
              result=num1/num2
              print ("The result is",result)

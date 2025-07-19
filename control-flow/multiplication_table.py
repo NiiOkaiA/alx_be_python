8cccf36b86562e3aa698ce1324dbbfed3f9ae754
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-07-19 05:41:20 +0000

    removed str from number

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
 number=int(input("enter a number to see its multiplication table:"))
 
 for a in range(1,11):
-    print (str(number),"*",str(a) ,"=" , number*a)
+    print(number,"*",str(a) ,"=" , number*a)

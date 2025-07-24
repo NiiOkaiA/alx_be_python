3372185384792bad8305fa609f03f3113b32456f
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-07-24 22:01:26 +0000

    changed logic to if num2 ==0

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,11 +9,13 @@ def perform_operation(num1, num2, operation):
         return num1*num2
 
     elif operation=="divide":
-        if num2! = 0:
-            return num1/num2
+        if num2 == 0:
+             print("cannot divide by 0")
+             exit()
+           
         else:
-            print("cannot divide by 0")
-            exit()
+             return num1/num2
+           
         
 
 

afe19ef8916cf7dc24fcad7e087c458f421ac22d
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-07-24 21:56:45 +0000

    changed case to if elif

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,24 @@
 def perform_operation(num1, num2, operation):
+    if operation=="add":
+        return num1+num2
+
+    elif operation=="subtract":
+        return num1-num2
+
+    elif operation=="multiply":
+        return num1*num2
+
+    elif operation=="divide":
+        if num2!=0:
+            return num1/num2
+        else:
+            print("cannot divide by 0")
+            exit()
+        
+
+
+    
+    '''
     match operation:
         case 'add':
             return num1+num2
@@ -14,7 +34,7 @@ def perform_operation(num1, num2, operation):
                       return num1/num2
             else:
                 print("cannot divide by 0")
-
+'''
 
     
 

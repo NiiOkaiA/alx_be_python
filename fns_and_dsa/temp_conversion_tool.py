d33b07f09373a6108d48c203addb339ae0f6f318
NiiOkaiA <niiokaiamarfio@gmail.com>
2025-07-26 22:22:38 +0000

    changed farenheit to fahrenheit

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,27 +1,27 @@
-FARENHEIT_TO_CELCIUS_FACTOR= 5/9
-CELCIUS_TO_FARENHEIT_FACTOR= 9/5
+FAHRENHEIT_TO_CELCIUS_FACTOR= 5/9
+CELCIUS_TO_FAHRENHEIT_FACTOR= 9/5
 
 
 
-def convert_to_celcius(farenheit):
-    celcius=FARENHEIT_TO_CELCIUS_FACTOR*farenheit
-    print(f"{farenheit} farenheit is {celcius} celcius")
+def convert_to_celcius(fahrenheit):
+    celcius=FAHRENHEIT_TO_CELCIUS_FACTOR*fahrenheit
+    print(f"{fahrenheit} fahrenheit is {celcius} celcius")
 
 
 def convert_to_farenheit(celcius):
-    farenheit=CELCIUS_TO_FARENHEIT_FACTOR*celcius
-    print(f"{celcius} celcius is {farenheit} farenheit")
+    fahrenheit=CELCIUS_TO_FAHRENHEIT_FACTOR*celcius
+    print(f"{celcius} celcius is {fahrenheit} fahrenheit")
 
 
 number=int(input("Enter the temprature to convert:"))
 
-celfar=input("Is this temprature in Celcius or Farenheit (C/F):")
+celfar=input("Is this temprature in Celcius or Fahrenheit (C/F):")
 
 if celfar=='C':
-    convert_to_farenheit(number)
+    convert_to_fahrenheit(number)
 
 
 if celfar=='F':
-    farenheit_to_celcius(number)
+    convert_to_celcius(number)
 
     
